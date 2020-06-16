09b0f2cfdc8119718e2b4c8811df06a3e2b1f681
Valdemir Bezerra de Souza Junior <52362288+ValdemirBSJr@users.noreply.github.com>
2020-06-16 18:33:21 -0300

    script python para logar no box automaticamente
    script python para logar no box automaticamente.
    Quando inicio o PC ele já loga para ser aberto uma instancia no laucher de arquivos

--- pySIMPLEGUI/exec_box_modulos.py
+++ pySIMPLEGUI/exec_box_modulos.py
@@ -2,28 +2,61 @@
 # -*- coding: utf-8 -*-
 #Author: Valdemir Bezerra
 
-import PySimpleGUI as sg
+
 import os
-import subprocess as sub
-import pexpect
+import pexpect as pxp
+import shelve
+import sys
 
 class Ativa_dav:
 
+    '''
+    Slot aloca na memoria apenas espaço para as 2 variaveis.
+    A classe recebe os attr de login e senha do arquivo shelve para logar
+    '''
+
     __slots__ = ['_login', '_senha']
 
-    def __init__(self, login='meulogin', senha='minhasenha'):
+    def __init__(self, login='', senha=''):
         self._login = login
         self._senha = senha
 
 
+
 if __name__ == '__main__':
 
-    input = Ativa_dav()
+    print('Tentando logar!...')
+    try:
 
-    comando = pexpect.spawn('gio mount davs://dav.box.com')
-    comando.expect('User: ')
-    comando.sendline(input._login)
-    comando.expect('Password: ')
-    comando.sendline(input._senha)
+        #Crio o arquivo shelve e lhe imputo valores
+        arquivoConf = shelve.open(os.path.join('db'))
+        #login=''
+        #senha=''
+        #arquivoConf['login'] = login
+        #arquivoConf['senha'] = senha
+        #print('Login: {}. Senha: {}'.format(arquivoConf['login'], arquivoConf['senha']))
+
+
+
+        input = Ativa_dav()
+        input._login = arquivoConf['login']
+        input._senha = arquivoConf['senha']
+
+        comando = pxp.spawn('gio mount davs://dav.box.com')
+        comando.expect('User: ')
+        comando.sendline(input._login)
+        comando.expect('Password: ')
+        comando.sendline(input._senha)
+
+        arquivoConf.close()
+
+        print('Loguei, olha lá!!!')
+
+    except Exception as erro:
+        print('Não foi possível logar no Dav do BOX! Erro: ', erro)
+        arquivoConf.close()
+        sys.exit()
+
+
 
 
