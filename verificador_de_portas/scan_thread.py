367191539a8606f44f5ef050af27eef2608eab64
Valdemir Bezerra de Souza Junior <52362288+ValdemirBSJr@users.noreply.github.com>
2020-09-26 21:50:47 -0300

    Update scan_thread.py

--- verificador_de_portas/scan_thread.py
+++ verificador_de_portas/scan_thread.py
@@ -1,4 +1,4 @@
-#!/home/valdemir/Documentos/PYTHON-PROJETOS/verificador_de_portas/venv/bin/python /home/valdemir/Documentos/PYTHON-PROJETOS/verificador_de_portas/scan_port.py
+#!/home/env python3
 # -*- coding: utf-8 -*-
 #Author: Valdemir Bezerra
 
