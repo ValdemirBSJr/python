6eaafdf55ddea58cfffbb29d7cd7155dcbc161f6
Valdemir Bezerra de Souza Junior <52362288+ValdemirBSJr@users.noreply.github.com>
2020-10-19 06:45:02 -0300

    Update enumerate_&_zip.py

--- enumerate_&_zip.py
+++ enumerate_&_zip.py
@@ -14,10 +14,31 @@ for sabor in lista_sabores:
 #enumerate é a função nativa para iterar por estruturas
 #de dados e pegar seu indice. Pode usar até o numero de inicio da contagem
 #se nao especificar começa do zero
+#ele atribui um indice e itera sobre a key
 for i, sabor in enumerate(lista_sabores, 1):
     print(f'Índice:{i} sabor: {sabor}')
 
 #############################################
+
+#para retornar tanto sobre chave e valor de um dicionario
+#use as formas abaixo:
+
+#percorre as keys:
+for key in dicionario:
+  print(key)
+
+#exibe os valores
+for key in dicionario:
+    print(dicionario[key])
+    
+#exibe os valores de outra maneira
+for key in dicionario.values():
+  print(key)
+
+#exibe chave e valores:
+
+for key, values in dicionario.items():
+  print(key, values)
 
 #Para iterar em 2 ou mais listas em paralelo, usar zip
 #zip usa tuplas contendo os valores da iteração
